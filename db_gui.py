e01d0b160b92e606348768bdbbc2272f0453afbe
jrbostic <jesse.r.bostic@gmail.com>
2014-12-19 17:12:58 -0800

    Initial commit

---
+++ db_gui.py
@@ -0,0 +1,121 @@
+from Tkinter import *
+import db_manager
+
+__author__ = 'jessebostic'
+
+class DisplayWindow:
+
+    def __init__(self):
+        self.root = Tk()
+
+        self.inner_window = Frame(self.root)
+        self.inner_window.pack()
+
+        self.entry = Entry(self.inner_window)
+        self.entry.grid(column=0, row=0, sticky=NSEW)
+
+        self.button = Button(self.inner_window, text="FIND CONTRIBUTOR", command=self.create_selection_window)
+        self.button.grid(column=1, row=0, sticky=NSEW)
+
+        self.canvas = Canvas(self.root, width=800, height=500)
+        self.canvas.pack()
+
+        self.select_list = None
+        self.selected_contributor = None
+        self.contributor = None
+        self.contributions = None
+        self.lobbies = None
+
+        #self.render_display()
+
+        self.root.mainloop()
+
+    def render_display(self):
+
+        self.canvas.delete('all')
+
+        if self.selected_contributor is not None:
+
+            contribution_percent = self.contributor[1]/(self.contributor[1]+self.contributor[2])*100
+            contribution_slice = self.contributor[1]/(self.contributor[1]+self.contributor[2])*359.999
+            lobby_slice = -359.999 + contribution_slice
+
+            if contribution_slice > 0.01:
+                self.canvas.create_arc(self.root.winfo_width()/2-50, self.root.winfo_height()/2-50,
+                                       self.root.winfo_width()/2+50, self.root.winfo_height()/2+50,
+                                       start=0, extent=contribution_slice, fill='blue',
+                                       activefill='green', outline='white', activeoutline='black', style=PIESLICE)
+                self.canvas.create_text(self.root.winfo_width()/2+26, self.root.winfo_height()/2-5,
+                                        text='{:.1f}%'.format(contribution_percent), fill='blue')
+
+            if lobby_slice < -0.01:
+                self.canvas.create_arc(self.root.winfo_width()/2-50, self.root.winfo_height()/2-50,
+                                       self.root.winfo_width()/2+50, self.root.winfo_height()/2+50,
+                                       start=0, extent=lobby_slice, fill='red',
+                                       activefill='green', outline='white', activeoutline='black', style=PIESLICE)
+                self.canvas.create_text(self.root.winfo_width()/2+26, self.root.winfo_height()/2+10,
+                                        text='{:.1f}%'.format(100-contribution_percent), fill='red')
+
+            self.canvas.create_text(self.root.winfo_width()/2, self.root.winfo_height()/4,
+                                    text=self.selected_contributor)
+            self.canvas.create_text(self.root.winfo_width()/2, self.root.winfo_height()/4+20,
+                                    text='Total: ${:,.2f}'.format(self.contributor[1]+self.contributor[2]))
+            self.canvas.create_text(self.root.winfo_width()/5, self.root.winfo_height()/5,
+                                    text='Contributions: ${:,.2f}'.format(self.contributor[1]))
+
+            listbox1 = Listbox(self.canvas, width=30, height=20)
+            listbox1.place(relx=.05, rely=.25)
+            for entry in self.contributions:
+                listbox1.insert(END, entry[0])
+
+            self.canvas.create_text((self.root.winfo_width()/5)*4, self.root.winfo_height()/5,
+                                    text='Lobbies: ${:,.2f}'.format(self.contributor[2]))
+
+            listbox2 = Listbox(self.canvas, width=30, height=20)
+            listbox2.place(relx=.65, rely=.25)
+            for entry in self.lobbies:
+                listbox2.insert(END, entry[0])
+
+        #self.canvas.after(100, self.render_display)
+
+    def create_selection_window(self):
+
+        def set_selection(select_list):
+            self.selected_contributor = select_list.get(select_list.curselection())
+            self.contributor = db_manager.get_contributor(self.selected_contributor)
+            self.contributions = db_manager.get_contributions(self.selected_contributor)
+            self.lobbies = db_manager.get_lobbies(self.selected_contributor)
+
+            self.render_display()
+
+            print self.contributor
+            print self.contributions
+            print self.lobbies
+
+            scroll_window.destroy()
+
+        results = db_manager.get_contributor_names(self.entry.get())
+
+        scroll_window = Toplevel(self.root)
+        scroll_window.geometry("%dx%d%+d%+d" % (500, 200, self.canvas.winfo_rootx()+10, self.canvas.winfo_rooty()))
+
+        inner_window = Frame(scroll_window)
+        inner_window.pack(fill=BOTH)
+
+        scrollbar = Scrollbar(inner_window, orient=VERTICAL)
+        xscrollbar = Scrollbar(inner_window, orient=HORIZONTAL)
+        self.select_list = Listbox(inner_window, yscrollcommand=scrollbar.set, xscrollcommand=xscrollbar.set)
+        scrollbar.config(command=self.select_list.yview)
+        scrollbar.pack(side=RIGHT, fill=Y)
+        xscrollbar.config(command=self.select_list.xview)
+        xscrollbar.pack(side=BOTTOM, fill=X)
+        for item in results:
+            self.select_list.insert(END, item)
+        self.select_list.pack(side=LEFT, fill=BOTH, expand=1)
+
+        #currently destroy window... need to return/set selection
+        Button(scroll_window, text="Select", command=lambda sel=self.select_list: set_selection(sel)).pack(side=LEFT, expand=1)
+        Button(scroll_window, text="Cancel", command=scroll_window.destroy).pack(side=RIGHT, expand=1)
+
+if __name__ == "__main__":
+    DisplayWindow()
---
+++ db_manager.py
@@ -0,0 +1,119 @@
+import sqlite3
+
+__author__ = 'jessebostic'
+
+
+def open_conn():
+    conn = sqlite3.connect('influence.db')
+    cursor = conn.cursor()
+    return conn, cursor
+
+
+def close_conn(conn, cursor):
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
+def create_tables():
+    conn, cursor = open_conn()
+    cursor.execute('CREATE TABLE CONTRIBUTORS (contributor_name text, contribution_total real, lobby_total real)')
+    cursor.execute('CREATE TABLE CONTRIBUTIONS (contributor_name text, recipient_name text, contribution_amount real)')
+    cursor.execute('CREATE TABLE LOBBIES (contributor_name text, recipient_name text)')
+    close_conn(conn, cursor)
+
+
+def insert_contribution(values, cursor):
+    """Inserts or updates entries in CONTRIBUTOR and CONTRIBUTIONS tables"""
+
+    # update CONTRIBUTORS table
+    contributor_entry = cursor.execute('SELECT * FROM CONTRIBUTORS '
+                                       'WHERE contributor_name = ? LIMIT 1', (values[0],)).fetchone()
+    if contributor_entry:
+        cursor.execute('UPDATE CONTRIBUTORS SET contribution_total = ? WHERE contributor_name = ?',
+                       (values[2]+contributor_entry[1], values[0]))
+    else:
+        cursor.execute('INSERT INTO CONTRIBUTORS VALUES (?,?,0.0)', (values[0], values[2]))
+
+    # update CONTRIBUTIONS table
+    contribution_entry = cursor.execute('SELECT * FROM CONTRIBUTIONS '
+                                        'WHERE contributor_name = ? AND recipient_name = ? LIMIT 1',
+                                        (values[0], values[1])).fetchone()
+    if contribution_entry:
+        cursor.execute('UPDATE CONTRIBUTIONS SET contribution_amount = ? '
+                       'WHERE contributor_name = ? AND recipient_name = ?',
+                       (values[2]+contribution_entry[2], values[0], values[1]))
+    else:
+        cursor.execute('INSERT INTO CONTRIBUTIONS VALUES (?, ?, ?)', values)
+
+
+
+def insert_lobby(values, cursor):
+    """Inserts, updates, or replaces entries in CONTRIBUTOR and LOBBIES tables"""
+
+    # update CONTRIBUTORS table (only adding $ once since multiple target agencies possible in single lobby)
+    contributor_entry = cursor.execute('SELECT * FROM CONTRIBUTORS '
+                                       'WHERE contributor_name = ? LIMIT 1', (values[0],)).fetchone()
+    if contributor_entry:
+        cursor.execute('UPDATE CONTRIBUTORS SET lobby_total = ? WHERE contributor_name = ?',
+                       (values[2]+contributor_entry[2], values[0]))
+    else:
+        cursor.execute('INSERT INTO CONTRIBUTORS VALUES (?,0.0,?)', (values[0], values[2]))
+
+    # update LOBBIES table
+    for i in xrange(len(values[1])):
+        lobby_entry = cursor.execute('SELECT * FROM LOBBIES WHERE contributor_name = ? AND recipient_name = ? LIMIT 1',
+                                     (values[0], values[1][i])).fetchone()
+        if lobby_entry is None:
+            cursor.execute('INSERT INTO LOBBIES VALUES (?,?)', (values[0], values[1][i]))
+
+
+def get_contributor_names(like):
+    conn, cursor = open_conn()
+    cursor.execute('SELECT contributor_name FROM CONTRIBUTORS '
+                   'WHERE contributor_name LIKE ("%"||?||"%") ORDER BY contributor_name', (like,))
+    name_list = cursor.fetchall()
+    close_conn(conn, cursor)
+    return [entry[0] for entry in name_list]
+
+
+def get_contributor(name):
+    conn, cursor = open_conn()
+    cursor.execute('SELECT * FROM CONTRIBUTORS WHERE contributor_name = ?', (name,))
+    contributor = cursor.fetchone()
+    close_conn(conn, cursor)
+    return contributor
+
+
+def get_contributions(name):
+    conn, cursor = open_conn()
+    cursor.execute('SELECT recipient_name, contribution_amount FROM CONTRIBUTIONS '
+                   'WHERE contributor_name = ? ORDER BY recipient_name', (name,))
+    contributions = cursor.fetchall()
+    close_conn(conn, cursor)
+    return contributions
+
+
+def get_lobbies(name):
+    conn, cursor = open_conn()
+    cursor.execute('SELECT recipient_name FROM LOBBIES WHERE contributor_name = ? ORDER BY recipient_name', (name,))
+    lobbies = cursor.fetchall()
+    close_conn(conn, cursor)
+    return lobbies
+
+
+
+conn, cursor = open_conn()
+# cursor.execute('SELECT COUNT(*) FROM CONTRIBUTORS')
+# print cursor.fetchall()
+# cursor.execute('SELECT COUNT(*) FROM CONTRIBUTIONS')
+# print cursor.fetchall()
+# cursor.execute('SELECT COUNT(*) FROM LOBBIES')
+# print cursor.fetchall()
+# cursor.execute('SELECT * FROM LOBBIES WHERE contributor_name like "%Dairy Farmers of America%"')
+# for each in cursor.fetchall():
+#     print each
+get_contributor_names('')
+close_conn(conn, cursor)
+
+
