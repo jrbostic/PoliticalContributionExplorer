81597ba241be58ad2c36a222b03d01c5261d5c79
jrbostic <jesse.r.bostic@gmail.com>
2014-12-20 15:24:29 -0800

    Added more GUI functionality, tweaked arrangement.  Commented out gui and builder files.

--- db_builder.py
+++ db_builder.py
@@ -1,68 +1,93 @@
+"""This file should only be run as __main__ where no influence.db exists in project directory.
+The constants defined at top of file are used by this process to determine what parameters will
+be sent to api in the db creation script.
+
+May be imported to gain access to json api call wrappers for retrieving political contributions and lobbies.
+"""
+
 import urllib
 import ujson as json
 import db_manager
 
 __author__ = 'jessebostic'
 
-MAX_PAGES = 200
-YEAR = '2012'
+# the api key to use while running script
+API_KEY = '37fcf779b2674edba2ca46da7277c3be'
+
+# upper bound for number of pages for script to run through
+MAX_PAGES = 250
+
+# the year for which results are desired
+YEAR = '2013'
+
+# the minimum contribution or lobby dollars relevant
 MIN_AMOUNT = 10000
 
 
-def get_contributions(page, year, per_page=1000, min_amount=0):
-    result = urllib.urlopen('http://transparencydata.com/api/1.0/contributions.json?apikey=' +
-                            '37fcf779b2674edba2ca46da7277c3be&page={}&per_page={}&cycle={}&amount=>|{}'
-                            .format(page, per_page, year, min_amount))
-    result_list = json.load(result)
-    return result_list
+def get_contributions(api_key, page, year, per_page=1000, min_amount=0):
+    """Returns list of dictionaries representing respective political contribution records.
 
-def get_lobbies(page, year, per_page=100, min_amount=0):
-    result = urllib.urlopen('http://transparencydata.com/api/1.0/lobbying.json?apikey=' +
-                            '37fcf779b2674edba2ca46da7277c3be&page={}&per_page={}&year={}&amount=>|{}'
-                            .format(page, per_page, year, min_amount))
-    the_result_list = json.load(result)
-    return the_result_list
+    :param api_key: the api key to use on access
+    :param page: the page number desired
+    :param year: the year of record desired
+    :param per_page: the number of results per page desired
+    :param min_amount: the minimum dollar contribution desired
+    :return: list of dicts representing individual political contributions
+    """
+
+    result = json.load(urllib.urlopen('http://transparencydata.com/api/1.0/contributions.json?apikey=' +
+                                      '{}&page={}&per_page={}&cycle={}&amount=>|{}'
+                                      .format(api_key, page, per_page, year, min_amount)))
+    return result
+
+def get_lobbies(api_key, page, year, per_page=100, min_amount=0):
+    """Returns list of dictionaries representing respective political lobby records.
+
+    :param api_key: the api key to use on access
+    :param page: the page number desired
+    :param year: the year of record desired
+    :param per_page: the number of results per page desired
+    :param min_amount: the minimum dollar lobby desired
+    :return: list of dicts representing individual lobby instances
+    """
+
+    result = json.load(urllib.urlopen('http://transparencydata.com/api/1.0/lobbying.json?apikey=' +
+                                      '{}&page={}&per_page={}&year={}&amount=>|{}'
+                                      .format(api_key, page, per_page, year, min_amount)))
+    return result
 
 if __name__ == "__main__":
+
     db_manager.create_tables()
 
-    # grab contributions
+    # grab contributions and insert into db
     for i in xrange(1, MAX_PAGES + 1):
 
-        print 'Contribution (Page {})'.format(i)
-        result_list = get_contributions(i, YEAR, min_amount=MIN_AMOUNT)
-        # result = urllib.urlopen('http://transparencydata.com/api/1.0/contributions.json?apikey=' +
-        #                         '37fcf779b2674edba2ca46da7277c3be&page={}&cycle={}&amount=>|{}'.format(i, YEAR, MIN_AMOUNT))
-        #
-        # print 'Loading...'
-        # the_result_list = json.load(result)
-
+        print('Contribution (Page {})'.format(i))
+        print('Retrieving and Loading...')
+        result_list = get_contributions(API_KEY, i, YEAR, min_amount=MIN_AMOUNT)
         if len(result_list) == 0:
             break
 
-        print 'Populating...'
+        print('Populating...')
         conn, cursor = db_manager.open_conn()
         for contribution in result_list:
             db_manager.insert_contribution((contribution['contributor_name'], contribution['recipient_name'],
-                                 float(contribution['amount'])), cursor)
+                                            float(contribution['amount'])), cursor)
         db_manager.close_conn(conn, cursor)
 
-    # grab lobbies
+    # grab lobbies and insert into db
     for i in xrange(1, MAX_PAGES + 1):
 
-        print 'Lobby (Page {})'.format(i)
-        result_list = get_lobbies(i, YEAR, min_amount=MIN_AMOUNT)
-        # result = urllib.urlopen('http://transparencydata.com/api/1.0/lobbying.json?apikey=' +
-        #                         '37fcf779b2674edba2ca46da7277c3be&page={}&year={}&amount=>|{}'.format(i, YEAR, MIN_AMOUNT))
-        #
-        # print 'Loading...'
-        # the_result_list = json.load(result)
+        print('Lobby (Page {})'.format(i))
+        print('Retrieving and Loading...')
+        result_list = get_lobbies(API_KEY, i, YEAR, min_amount=MIN_AMOUNT)
         if len(result_list) == 0:
             break
 
-        print 'Populating...'
+        print('Populating...')
         conn, cursor = db_manager.open_conn()
         for lobby in result_list:
             db_manager.insert_lobby((lobby['client_name'], [agency['agency_name'] for agency in lobby['agencies']],
-                          float(lobby['amount'])), cursor)
+                                    float(lobby['amount'])), cursor)
         db_manager.close_conn(conn, cursor)
--- db_gui.py
+++ db_gui.py
@@ -1,19 +1,34 @@
+"""Provides GUI for exploring political influence database."""
+
 from Tkinter import *
 import db_manager
 
 __author__ = 'jessebostic'
 
 class DisplayWindow:
+    """Class providing a graphical interface for exploring specific contributor
+    records residing in political influence database.
+    """
 
     def __init__(self):
+        """Sets up tkinter components, geometry managers, bindings, and instance-level members.
+
+        :return: new DisplayWindow instance
+        """
+
+        # master widget
         self.root = Tk()
         self.root.resizable(0, 0)
+        self.root.title("Political Influence Explorer")
 
+        # frame for upper entry box and search button
         self.inner_window = Frame(self.root)
         self.inner_window.pack()
 
         self.entry = Entry(self.inner_window)
         self.entry.grid(column=0, row=0, sticky=NSEW)
+        self.entry.bind('<Return>', self.create_selection_window)
+        self.entry.focus_set()
 
         self.button = Button(self.inner_window, text="FIND CONTRIBUTOR", command=self.create_selection_window)
         self.button.grid(column=1, row=0, sticky=NSEW)
@@ -21,31 +36,38 @@ class DisplayWindow:
         self.canvas = Canvas(self.root, width=800, height=500)
         self.canvas.pack()
 
+        # various members for tracking current selection
         self.select_list = None
         self.selected_contributor = None
+        self.recipient_listbox = None
         self.contributor = None
         self.contributions = None
         self.lobbies = None
 
-        #self.render_display()
-
         self.root.mainloop()
 
     def render_display(self):
+        """Updates all graphical contents of canvas.  Uses clunky placement calculations which could/should
+        be simplified and refactored on next iteration.
+         """
 
         self.canvas.delete('all')
 
         if self.selected_contributor is not None:
 
-            ratio = 1
-            if self.contributor[1] != 0 != self.contributor[2]:
+            # get ratio of contribution/(contribution+lobby)
+            #  avoiding division by zero
+            ratio = 0
+            if self.contributor[1] != 0 or self.contributor[2] != 0:
                 ratio = self.contributor[1]/(self.contributor[1]+self.contributor[2])
 
+            # extract pie degrees from ratio
             contribution_percent = ratio*100
             contribution_slice = ratio*359.999
             lobby_slice = -359.999 + contribution_slice
 
-            if contribution_slice > 0.01:
+            # create pie slices in center of window
+            if contribution_slice > 0.001:
                 self.canvas.create_arc(self.root.winfo_width()/2-50, self.root.winfo_height()/2-50,
                                        self.root.winfo_width()/2+50, self.root.winfo_height()/2+50,
                                        start=0, extent=contribution_slice, fill='blue',
@@ -53,7 +75,7 @@ class DisplayWindow:
                 self.canvas.create_text(self.root.winfo_width()/2+26, self.root.winfo_height()/2-5,
                                         text='{:.1f}%'.format(contribution_percent), fill='blue')
 
-            if lobby_slice < -0.01:
+            if lobby_slice < -0.001:
                 self.canvas.create_arc(self.root.winfo_width()/2-50, self.root.winfo_height()/2-50,
                                        self.root.winfo_width()/2+50, self.root.winfo_height()/2+50,
                                        start=0, extent=lobby_slice, fill='red',
@@ -61,48 +83,96 @@ class DisplayWindow:
                 self.canvas.create_text(self.root.winfo_width()/2+26, self.root.winfo_height()/2+10,
                                         text='{:.1f}%'.format(100-contribution_percent), fill='red')
 
-            self.canvas.create_text(self.root.winfo_width()/2, self.root.winfo_height()/4,
+            # non-piechart display elements in center of window
+            self.canvas.create_text(self.root.winfo_width()/2, self.root.winfo_height()/10,
                                     text=self.selected_contributor)
-            self.canvas.create_text(self.root.winfo_width()/2, self.root.winfo_height()/4+20,
+            self.canvas.create_text(self.root.winfo_width()/2, self.root.winfo_height()/10+20,
                                     text='Total: ${:,.2f}'.format(self.contributor[1]+self.contributor[2]))
+            self.canvas.create_rectangle((self.root.winfo_width()/10)*4, (self.root.winfo_height()/10)*7,
+                                        (self.root.winfo_width()/10)*4+20, (self.root.winfo_height()/10)*7+15,
+                                         fill='blue')
+            self.canvas.create_text((self.root.winfo_width()/10)*5, (self.root.winfo_height()/10)*7+8,
+                                    text='Contributions')
+            self.canvas.create_rectangle((self.root.winfo_width()/10)*4, (self.root.winfo_height()/10)*7.5,
+                                        (self.root.winfo_width()/10)*4+20, (self.root.winfo_height()/10)*7.5+15,
+                                         fill='red')
+            self.canvas.create_text((self.root.winfo_width()/10)*5, (self.root.winfo_height()/10)*7.5+8, text='Lobbies')
+
+            # contributions arrangement at left of window
             self.canvas.create_text(self.root.winfo_width()/5, self.root.winfo_height()/5,
                                     text='Contributions: ${:,.2f}'.format(self.contributor[1]))
-
-            listbox1 = Listbox(self.canvas, width=30, height=20)
-            listbox1.place(relx=.05, rely=.25)
+            self.recipient_listbox = Listbox(self.canvas, width=30, height=20)
+            self.recipient_listbox.place(relx=.05, rely=.25)
             for entry in self.contributions:
-                listbox1.insert(END, entry[0])
+                self.recipient_listbox.insert(END, entry[0])
+            self.recipient_listbox.bind("<Double-Button-1>", self.show_recipient)
 
+            # lobbies arrangement at right of window
             self.canvas.create_text((self.root.winfo_width()/5)*4, self.root.winfo_height()/5,
                                     text='Lobbies: ${:,.2f}'.format(self.contributor[2]))
-
-            listbox2 = Listbox(self.canvas, width=30, height=20)
-            listbox2.place(relx=.65, rely=.25)
+            agency_listbox = Listbox(self.canvas, width=30, height=20)
+            agency_listbox.place(relx=.65, rely=.25)
             for entry in self.lobbies:
-                listbox2.insert(END, entry[0])
+                agency_listbox.insert(END, entry[0])
 
-        #self.canvas.after(100, self.render_display)
+    def show_recipient(self, event=None):
+        """Shows small popup window with recipient name and specific contribution amount.
 
-    def create_selection_window(self):
+        :param event: allows key-bound callbacks (not used in function)
+        """
+
+        # set up window and geometry
+        recipient_window = Toplevel(self.recipient_listbox)
+        recipient_window.title("Contribution Detail")
+
+        x = self.recipient_listbox.winfo_rootx()+10
+        y = self.recipient_listbox.winfo_rooty()/5
+        height = self.recipient_listbox.winfo_height()
+        geom = "%dx%d+%d+%d" % (300, 100, x,y+height)
+        recipient_window.geometry(geom)
+
+        # retrieve and insert data particulars
+        recipient_name = self.recipient_listbox.get(self.recipient_listbox.curselection())
+        recipient_tuple = None
+        for entry in self.contributions:
+            if recipient_name == entry[0]:
+                recipient_tuple = entry
+        Label(recipient_window, text='{}'.format(recipient_tuple[0])).pack()
+        Label(recipient_window, text='${:,.2f}'.format(recipient_tuple[1])).pack()
+
+    def create_selection_window(self, event=None):
+        """Initializes display window for contributor selection based on text in main entry widget.
+
+        :param event: allows key-bound callbacks (not used in function)
+        """
 
         def set_selection(select_list):
-            self.selected_contributor = select_list.get(select_list.curselection())
-            self.contributor = db_manager.get_contributor(self.selected_contributor)
-            self.contributions = db_manager.get_contributions(self.selected_contributor)
-            self.lobbies = db_manager.get_lobbies(self.selected_contributor)
+            """If an item has been selected in Listbox, sets DisplayWindow instance fields to appropriate values,
+            fires off graphical display repaint, and destroys selection window.
 
-            self.render_display()
+            :param select_list: contributor Listbox to get user selection from
+            """
 
-            print self.contributor
-            print self.contributions
-            print self.lobbies
+            if select_list.curselection() != ():
+                self.selected_contributor = select_list.get(select_list.curselection())
+                self.contributor = db_manager.get_contributor(self.selected_contributor)
+                self.contributions = db_manager.get_contributions(self.selected_contributor)
+                self.lobbies = db_manager.get_lobbies(self.selected_contributor)
 
-            scroll_window.destroy()
+                self.render_display()
+
+                print self.contributor
+                print self.contributions
+                print self.lobbies
+
+                scroll_window.destroy()
 
         results = db_manager.get_contributor_names(self.entry.get())
 
+        # creates selection window along with x and y scroll bars
         scroll_window = Toplevel(self.root)
-        scroll_window.geometry("%dx%d%+d%+d" % (500, 200, self.canvas.winfo_rootx()+10, self.canvas.winfo_rooty()))
+        scroll_window.geometry('%dx%d%+d%+d' % (500, 200, self.canvas.winfo_rootx()+10, self.canvas.winfo_rooty()))
+        scroll_window.title('Select Contributor')
 
         inner_window = Frame(scroll_window)
         inner_window.pack(fill=BOTH)
@@ -114,12 +184,14 @@ class DisplayWindow:
         scrollbar.pack(side=RIGHT, fill=Y)
         xscrollbar.config(command=self.select_list.xview)
         xscrollbar.pack(side=BOTTOM, fill=X)
+
+        # populate contributors into listbox
         for item in results:
             self.select_list.insert(END, item)
         self.select_list.pack(side=LEFT, fill=BOTH, expand=1)
 
-        #currently destroy window... need to return/set selection
-        Button(scroll_window, text="Select", command=lambda sel=self.select_list: set_selection(sel)).pack(side=LEFT, expand=1)
+        Button(scroll_window, text="Select",
+               command=lambda sel=self.select_list: set_selection(sel)).pack(side=LEFT, expand=1)
         Button(scroll_window, text="Cancel", command=scroll_window.destroy).pack(side=RIGHT, expand=1)
 
 if __name__ == "__main__":
